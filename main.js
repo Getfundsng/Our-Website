ddc6cc14e4882785d470a14f714fdc1b36b55a82
Sammydepoj <samueladepoju86@yahoo.com>
2022-07-14 20:29:59 +0100

    i don tire

--- main.js
+++ main.js
@@ -12,9 +12,39 @@ if (event.target == document.getElementById('modal')){
 }
 }
 
-
+// 
 let menu = document.getElementsByClassName('menu-icon')[0];
 menu.onclick=function(){
             document.getElementById('ul').style.display="block";
             menu.style.display="none"
 }
+
+
+
+
+// donation page form showing
+function show_form(){
+    if (document.getElementById('donate-form').style.display=='block'){
+        document.getElementById('donate-form').style.display='none'
+    }
+    else{
+        document.getElementById('donate-form').style.display='block';
+    }
+}
+
+// About page function
+
+function myFunction() {
+    var x = document.getElementById("thisTopnav");
+    if (x.className === "nav-list") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-list";
+    }
+    var x = document.getElementById("thisTopnavv");
+    if (x.className === "btn") {
+      x.className += " responsive";
+    } else {
+      x.className = "btn";
+    }
+  }
